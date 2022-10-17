faf7049e85ce19709245d06a157c573111f15fac
Fastoch <89261095+fastoch@users.noreply.github.com>
2022-10-17 15:34:20 +0200

    Create rmTheMin.js

---
+++ rmTheMin.js
@@ -0,0 +1,16 @@
+// Remove the minimum
+// https://www.codewars.com/kata/563cf89eb4747c5fb100001b/javascript
+
+/*
+Given an array of integers, remove the smallest value. Do not mutate the original array/list. 
+If there are multiple elements with the same value, remove the one with a lower index. 
+If you get an empty array/list, return an empty array/list.
+
+Don't change the order of the elements that are left.
+
+Examples
+* Input: [1,2,3,4,5], output= [2,3,4,5]
+* Input: [5,3,2,1,4], output = [5,3,2,4]
+* Input: [2,2,1,2,1], output = [2,2,2,1]
+*/
+
