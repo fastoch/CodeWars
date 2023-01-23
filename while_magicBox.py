64b631582684b822df3a871dc24dcfe2c521e588
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-01-23 13:49:34 +0100

    Update while_magicBox.py

--- while_magicBox.py
+++ while_magicBox.py
@@ -1,9 +1,9 @@
 # You have a magic box that doubles the count of items you put, in every day.
-# The given program takes the initial count of the items and the number of days as input. 
+# The program takes the initial count of the items and the number of days as input. 
 
 items = int(input())
 days = int(input())
-#your code goes here
+
 while days>0:
     items*=2
     days-=1
