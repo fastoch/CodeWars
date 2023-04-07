0e5638acfe215f543889c7ec5324a7aa7caa066b
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-04-07 12:06:24 +0200

    Create reversedArray.py

---
+++ reversedArray.py
@@ -0,0 +1,13 @@
+"""
+https://www.codewars.com/kata/5583090cbe83f4fd8c000051/train/python
+
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Examples(Input => Output):
+  35231 => [1,3,2,5,3]
+  0 => [0]
+
+"""
+
+def digitize(n):
+    return
