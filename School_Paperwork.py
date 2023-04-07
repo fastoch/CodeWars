bc736a6bb1a735d1614d22238f768a9f2dce824b
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-04-07 10:48:36 +0200

    Update School_Paperwork.py

--- School_Paperwork.py
+++ School_Paperwork.py
@@ -1,4 +1,5 @@
 """
+https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/train/python
 Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
 Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0
 """
