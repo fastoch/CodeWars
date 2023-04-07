138b9e32bbdf7dc01909195cfd2b603b672d2ee4
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-04-07 10:53:14 +0200

    Update String_repeat.py

--- String_repeat.py
+++ String_repeat.py
@@ -1,4 +1,7 @@
 """
 https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/python
-
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
 """
+
+def repeat_str(repeat, string):
+    return string * repeat
