d77dbb70feec54aa2ef005dc56d711298b466cd8
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-04-09 19:29:03 +0200

    Update SumArrays.py

--- SumArrays.py
+++ SumArrays.py
@@ -1,3 +1,8 @@
 """
 https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/python
-
+Write a function that takes an array of numbers and returns the sum of the numbers. 
+The numbers can be negative or non-integer. 
+If the array does not contain any numbers then you should return 0.
+"""
+def sum_array(a):
+    return sum(x for x in a)
