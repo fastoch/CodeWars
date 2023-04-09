2419134069e8d3ec20cf6974b2ca5c5e69660183
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-04-09 19:31:57 +0200

    Create DNAtoRNA.py

---
+++ DNAtoRNA.py
@@ -0,0 +1,18 @@
+"""
+https://www.codewars.com/kata/5556282156230d0e5e000089/train/python
+
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. 
+It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. 
+RNA differs slightly from DNA its chemical structure and contains no Thymine. 
+In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA.
+"""
+
+# my code
+def dna_to_rna(dna):
+    return
+  
+# alternative
