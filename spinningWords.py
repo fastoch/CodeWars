3aee2fb4e0b3dc9c0eb30d87618154bc5fa2bc90
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-04-10 12:13:28 +0200

    Create spinningWords.py

---
+++ spinningWords.py
@@ -0,0 +1,7 @@
+"""
+https://www.codewars.com/kata/5264d2b162488dc400000001/train/python
+
+Write a function that takes in a string of one or more words, and returns the same string, but with 
+all five or more letter words reversed (Just like the name of this Kata). 
+Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+"""
