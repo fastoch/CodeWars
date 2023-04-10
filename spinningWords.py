380ae4a537ff99ec25f7f35b09ef7b25c2b6a8c9
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-04-10 14:54:43 +0200

    Update spinningWords.py

--- spinningWords.py
+++ spinningWords.py
@@ -5,3 +5,17 @@ Write a function that takes in a string of one or more words, and returns the sa
 all five or more letter words reversed (Just like the name of this Kata). 
 Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
 """
+
+# my code
+def spin_words(sentence):  
+    liste = sentence.split()            # turns our sentence into a list of strings
+    new_liste =[]                       # will contain elements of our new_sentence
+    for x in liste:         
+        if len(x) >= 5:
+            x = x[::-1]                 # creates a slice that starts at the end of the string and moves backwards
+        new_liste.append(x)             # feeds my new list
+    new_sentence = ' '.join(new_liste)  # makes a new sentence with elements of my new list
+    return new_sentence
+  
+  # alternative
+  
